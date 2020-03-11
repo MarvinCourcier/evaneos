60db6ca8d1983cce5a6a84fbe1ac3c43a6d3dd3f
Marvin Courcier <courciermarvin@gmail.com>
2020-03-11 16:05:58 +0100

    Turning public variable to private, adding getters and setters, Documentation updated, adding types to construct params

--- src/Entity/Site.php
+++ src/Entity/Site.php
@@ -1,13 +1,86 @@
 <?php
 
+/**
+ * Class Site
+ *
+ * @category  Evaneos
+ * @package   Evaneos\Entity
+ * @author    Marvin Courcier <courciermarvin@gmail.com>
+ * @copyright 2020
+ */
 class Site
 {
-    public $id;
-    public $url;
+    /**
+     * Id
+     *
+     * @var int
+     */
+    private $id;
 
-    public function __construct($id, $url)
+    /**
+     * Url
+     *
+     * @var string
+     */
+    private $url;
+
+    /**
+     * Site Constructor.
+     *
+     * @param int    $id
+     * @param string $url
+     */
+    public function __construct(int $id, string $url)
+    {
+        $this->id  = $id;
+        $this->url = $url;
+    }
+
+    /**
+     * Get Id
+     *
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set Id
+     *
+     * @param integer $id
+     *
+     * @return self
+     */
+    public function setId(int $id): self
     {
         $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get Url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set Url
+     *
+     * @param string $url
+     *
+     * @return self
+     */
+    public function setUrl(string $url): self
+    {
         $this->url = $url;
+
+        return $this;
     }
 }
