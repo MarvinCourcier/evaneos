2845c9f8d906f668703bcc22ff788cc505b2ac97
Marvin Courcier <courciermarvin@gmail.com>
2020-03-11 16:39:35 +0100

    Turning public variable to private, adding getters and setters, Documentation updated, adding types to construct params

--- src/Entity/Template.php
+++ src/Entity/Template.php
@@ -1,15 +1,119 @@
 <?php
 
+/**
+ * Class Template
+ *
+ * @category  Evaneos
+ * @package   Evaneos\Entity
+ * @author    Marvin Courcier <courciermarvin@gmail.com>
+ * @copyright 2020
+ */
 class Template
 {
-    public $id;
-    public $subject;
-    public $content;
+    /**
+     * Id
+     *
+     * @var int
+     */
+    private $id;
 
-    public function __construct($id, $subject, $content)
+    /**
+     * Subject
+     *
+     * @var string
+     */
+    private $subject;
+
+    /**
+     * Content
+     *
+     * @var string
+     */
+    private $content;
+
+    /**
+     * Template Constructor.
+     *
+     * @param int    $id
+     * @param string $subject
+     * @param string $content
+     */
+    public function __construct(int $id, string $subject, string $content)
     {
-        $this->id = $id;
+        $this->id      = $id;
         $this->subject = $subject;
         $this->content = $content;
+    }
+
+    /**
+     * Get Id
+     *
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set Id
+     *
+     * @param integer $id
+     *
+     * @return self
+     */
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get Subject
+     *
+     * @return string
+     */
+    public function getSubject(): string
+    {
+        return $this->subject;
+    }
+
+    /**
+     * Set Subject
+     *
+     * @param string $subject
+     *
+     * @return self
+     */
+    public function setSubject(string $subject): self
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    /**
+     * Get Content
+     *
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set Content
+     *
+     * @param string $content
+     *
+     * @return self
+     */
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
     }
 }
