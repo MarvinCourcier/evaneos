27beba74a12de465c27d17065226384e41bccfc4
Marvin Courcier <courciermarvin@gmail.com>
2020-03-12 16:29:58 +0100

    Adding comment to function and attributes

--- src/Context/ApplicationContext.php
+++ src/Context/ApplicationContext.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Class ApplicationContext
+ *
+ * @category  Evaneos
+ * @package   Evaneos\Context
+ * @author    Marvin Courcier <courciermarvin@gmail.com>
+ * @copyright 2020
+ */
 class ApplicationContext
 {
     use SingletonTrait;
@@ -8,25 +16,67 @@ class ApplicationContext
      * @var Site
      */
     private $currentSite;
+
     /**
      * @var User
      */
     private $currentUser;
 
-    protected function __construct()
+    /**
+     * ApplicationContext Constructor.
+     */
+    public function __construct()
     {
         $faker = \Faker\Factory::create();
         $this->currentSite = new Site($faker->randomNumber(), $faker->url);
         $this->currentUser = new User($faker->randomNumber(), $faker->firstName, $faker->lastName, $faker->email);
     }
 
-    public function getCurrentSite()
+    /**
+     * Get Current Site
+     *
+     * @return Site|null
+     */
+    public function getCurrentSite(): ?Site
     {
         return $this->currentSite;
     }
 
+    /**
+     * Set Current Site
+     *
+     * @param Site $currentSite
+     *
+     * @return self
+     */
+    public function setCurrentSite(Site $currentSite)
+    {
+        $this->currentSite = $currentSite;
+
+        return $this;
+    }
+
+    /**
+     * Get Current User
+     *
+     * @return User
+     */
     public function getCurrentUser()
     {
         return $this->currentUser;
+    }
+
+    /**
+     * Set Current User
+     *
+     * @param User $currentUser
+     *
+     * @return self
+     */
+    public function setCurrentUser(User $currentUser)
+    {
+        $this->getCurrentUser = $currentUser;
+
+        return $this;
     }
 }
